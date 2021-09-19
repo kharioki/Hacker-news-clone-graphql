212d7f217f9db6265dfa069652321a5e3a23881c
tony <antolisah@gmail.com>
2021-09-19 16:30:38 +0300

    schema definition

--- src/index.js
+++ src/index.js
@@ -3,11 +3,33 @@ const { ApolloServer } = require('apollo-server');
 const typeDefs = `
   type Query {
     info: String!
+    feed: [Link!]!
+  }
+
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
   }
 `
+
+let links = [
+  {
+    id: 'link-0',
+    description: 'Fullstack GraphQL tutorial',
+    url: 'http://howtographql.com/'
+  }
+]
+
 const resolvers = {
   Query: {
-    info: () => 'This is the API of a Hackernews Clone'
+    info: () => 'This is the API of a Hackernews Clone',
+    feed: () => links
+  },
+  Link: {
+    id: (parent) => parent.id,
+    description: (parent) => parent.description,
+    url: (parent) => parent.url
   }
 }
 
