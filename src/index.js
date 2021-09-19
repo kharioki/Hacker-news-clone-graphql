a69971cbad41cf1f0762add19c88a37c6ea377f1
tony <antolisah@gmail.com>
2021-09-19 20:18:47 +0300

    connect sqlite with apollo server

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const { ApolloServer } = require('apollo-server');
 const fs = require('fs');
 const { join } = require('path');
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
 
 let links = [
   {
@@ -14,21 +17,21 @@ const resolvers = {
   // queries
   Query: {
     info: () => 'This is the API of a Hackernews Clone',
-    feed: () => links
+    feed: async (parent, args, context) => {
+      return context.prisma.link.findMany();
+    },
   },
 
   // mutations
   Mutation: {
-    post: (parent, args) => {
-      let idCount = links.length;
-
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url
-      };
-      links.push(link);
-      return link;
+    post: (parent, args, context, info) => {
+      const newLink = context.prisma.link.create({
+        data: {
+          url: args.url,
+          description: args.description,
+        },
+      })
+      return newLink;
     }
   },
 }
@@ -39,6 +42,9 @@ const server = new ApolloServer({
     'utf8'
   ),
   resolvers,
+  context: {
+    prisma,
+  }
 });
 
 server
