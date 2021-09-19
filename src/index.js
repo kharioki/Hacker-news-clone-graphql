a09328c5e73dcca878103544967349a0bf085977
tony <antolisah@gmail.com>
2021-09-19 17:34:40 +0300

    mutations

--- src/index.js
+++ src/index.js
@@ -11,15 +11,26 @@ let links = [
 ]
 
 const resolvers = {
+  // queries
   Query: {
     info: () => 'This is the API of a Hackernews Clone',
     feed: () => links
   },
-  Link: {
-    id: (parent) => parent.id,
-    description: (parent) => parent.description,
-    url: (parent) => parent.url
-  }
+
+  // mutations
+  Mutation: {
+    post: (parent, args) => {
+      let idCount = links.length;
+
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url
+      };
+      links.push(link);
+      return link;
+    }
+  },
 }
 
 const server = new ApolloServer({
@@ -27,7 +38,7 @@ const server = new ApolloServer({
     join(__dirname, 'schema.graphql'),
     'utf8'
   ),
-  resolvers
+  resolvers,
 });
 
 server
